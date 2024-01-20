933d7f95a657638cb8756d6efe67edf8b780ebf3
itslucassouza <lucasvini193@hotmail.com>
2024-01-20 18:46:12 -0300

    feat: add new components

--- src/presentation/pages/login.tsx
+++ src/presentation/pages/login.tsx
@@ -3,7 +3,10 @@ import './login-styles.scss';
 export function Login() {
   return (
     <div className="login">
-      <header>aa</header>
+      <header>
+        {/* <img src={} /> */}
+        <h1>4Dev - Enquetes para programadores</h1>
+      </header>
       <form className="form">aasdas</form>
       <footer></footer>
     </div>
